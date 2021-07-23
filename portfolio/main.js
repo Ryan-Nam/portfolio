b03623f2ac4c85c4af91569456bdf57c9663c4e4
Ryan-Nam <nam.changeworld@gmail.com>
2021-07-24 01:11:04 +1000

    add replacing an img on scroll

--- portfolio/main.js
+++ portfolio/main.js
@@ -2,18 +2,21 @@
 // track scrolling position
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
-const navbar__logo = document.querySelector('.navbar__logo__img');
 const navbarHeight = navbar.getBoundingClientRect().height;
+const navbar__logo = document.querySelector('.navbar__logo__img--dynamic');
 
 document.addEventListener('scroll', () => {
     // console.log(window.scrollY);
     // console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
-        navbar__logo.classList.add('navbar--changelogo');
+        navbar__logo.src="imgs/ryan_black.png"
+        
  
 
     }else{
         navbar.classList.remove('navbar--dark');
+        navbar__logo.src="imgs/ryan_white.png"
     }
 });
+
