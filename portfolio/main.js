86b0c64aea4fe65b8289780aadff8c31bcf25914
Ryan-Nam <nam.changeworld@gmail.com>
2021-07-23 23:51:01 +1000

    debug nav bar

--- portfolio/main.js
+++ portfolio/main.js
@@ -2,12 +2,16 @@
 // track scrolling position
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
+const navbar__logo = document.querySelector('.navbar__logo__img');
 const navbarHeight = navbar.getBoundingClientRect().height;
+
 document.addEventListener('scroll', () => {
     // console.log(window.scrollY);
     // console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
+        navbar__logo.classList.add('navbar--changelogo');
+ 
 
     }else{
         navbar.classList.remove('navbar--dark');
