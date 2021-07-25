ab2808137ef244beac2c66b74c143b51160d3164
Ryan-Nam <nam.changeworld@gmail.com>
2021-07-25 22:22:02 +1000

    Fix js for the dynamic effect - hamburger color

--- portfolio/main.js
+++ portfolio/main.js
@@ -5,18 +5,26 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 const navbar__logo = document.querySelector('.navbar__logo__img--dynamic');
 
+const hamburger__color = document.querySelector('.navbar__toggle-btn');
+
+
 document.addEventListener('scroll', () => {
     // console.log(window.scrollY);
     // console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
         navbar__logo.src="imgs/ryan_black.png"
+        hamburger__color.style.color = "var(--color-black)"
+        
+        
         
  
 
     }else{
         navbar.classList.remove('navbar--dark');
         navbar__logo.src="imgs/ryan_white.png"
+        
+        hamburger__color.style.color = "var(--color-white)"
     }
 });
 
