4effc73bfcf271de973c5524efe164ed0b0da82f
Ryan-Nam <nam.changeworld@gmail.com>
2021-07-26 23:24:51 +1000

    Add js for filtering active effects

--- portfolio/main.js
+++ portfolio/main.js
@@ -83,11 +83,14 @@ workBtncontainer.addEventListener('click', (e) =>{
     }
     // } if filter is null, do not do anything
     //console.log(filter);
+
+
+    //Remove selection from the previous item and select the new one
+    const active = document.querySelector('.category__btn.selected');
+    active.classList.remove('selected');
+    e.target.classList.add('selected');
+
     projectContainer.classList.add('anim-out');
-
-
-    
-
     setTimeout(() => {
 
         projects.forEach((project) => {
